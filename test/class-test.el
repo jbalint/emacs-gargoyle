27eeffde915b8e8f43996cde306fbf5ee79f24a6
Jess Balint <jbalint@gmail.com>
2016-07-29 20:17:50 -0500

    Misc doc updates [YT-32]

--- test/class-test.el
+++ test/class-test.el
@@ -1,4 +1,5 @@
 (ert-deftest basic-class-access-string ()
+  "Can we load a class given the class name as a string"
   (let* ((c (gg-find-class "java.util.ArrayList"))
 		 (c-as-string (gg-toString c))
 		 (class-name (gg-get-class-name c)))
@@ -6,23 +7,27 @@
 	(should (equal 'java.util.ArrayList class-name))))
 
 (ert-deftest basic-class-access-symbol ()
+  "Can we load a class given the class name as a symbol"
   :expected-result :failed
   (let* ((c (gg-find-class 'java.util.ArrayList))
 		 (c-as-string (gg-toString c)))
 	(should (string-equal "class java.util.ArrayList" c-as-string))))
 
 (ert-deftest no-class-def ()
+  "Does an attempt to load a non-existent class throw a proper exception?"
   (message "* The following NoClassDefError exception is expected:")
   (condition-case exc (gg-find-class "java.DoesntExist")
 	(java-exception (should (eq 'java-exception (car exc))))))
 
 (ert-deftest find-class-on-non-string ()
+  "Does an attempt to load a class given a wrong type throw a proper exception?"
   (condition-case err (gg-find-class nil)
 	(wrong-type-argument (should (string-equal "Expected string:" (cadr err)))))
   (condition-case err (gg-find-class 42)
 	(wrong-type-argument (should (string-equal "Expected string:" (cadr err))))))
 
 (ert-deftest new-instance-0arg ()
+  "Can we call a constructor with no arguments?"
   (let* ((o (gg-new "java.util.ArrayList"))
 		 (o-as-string (gg-toString o)))
 	(should (string-equal "[]" o-as-string)))
